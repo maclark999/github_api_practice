1a0a4717ef5dcaaac6af70a78f90e8a542d94d37
mackenzie clarkson <maclark99@gmail.com>
2015-03-11 13:04:15 -0600

    it lists your repositories

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # github_api_practice
+## Stories
+- Build a rails application that interacts with github.
+- It lists your repositories.
+- It lists your location.
+- text field that returns a searched user's information including Location, Created at, email, name, company, avatar.
+- text field that lists a searched users repositories.
--- app/models/github_fetcher.rb
+++ app/models/github_fetcher.rb
@@ -1,7 +1,14 @@
 class GithubFetcher
   attr_reader :client
 
-  def initialize
-    @client = Octokit::Client.new(Rails.application.secrets.github_access_token)
+  def initialize(github)
+    @github = github
+    @client = Octokit::Client.new do |config|
+      config.github_access_token = Rails.application.secrets.github_access_token
+    end
+  end
+
+  def github_repos
+    @client.repositories('maclark999')
   end
 end
